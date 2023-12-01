c7020fab6a42a3d2422baf8fc40008391ee327c2
Abu Osama <abuosamasports@gmail.com>
2023-12-01 23:30:59 +0530

    css modified

--- frontendd/src/Components/ContactList.jsx
+++ frontendd/src/Components/ContactList.jsx
@@ -1,18 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../Styles/ContactList.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchContacts } from "../Redux/action";
+import Loader from "../Components/Loader";
 
 function ContactList() {
   const contactsss = useSelector((store) => store.contactReducer.contacts);
+  const [loading, setLoading] = useState(true);
   console.log(contactsss);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
+    const fetchData = async () => {
+      try {
+        await dispatch(fetchContacts());
+      } finally {
+        setLoading(false);
+      }
+    };
 
+    fetchData();
+  }, [dispatch]);
+  if (loading) {
+    return (
+      <div>
+        <Loader />
+      </div>
+    );
+  }
   return (
     <div id="ContactList" className="ContactList">
       <div className="search-container">
@@ -23,6 +39,7 @@ function ContactList() {
           //   onChange={(e) => setSearchTerm(e.target.value)}
         />
       </div>
+
       {contactsss.map((item) => (
         <Link
           to={`/contact-details/${item._id}`}
---
+++ frontendd/src/Components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Styles/Loader.scss"
+
+function Loder() {
+  return (
+    <div className="spinner-loader">
+      <div className="loding-spinner"></div>
+    </div>
+  );
+}
+
+export default Loder;
